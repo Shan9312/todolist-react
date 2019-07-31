a13b52668c3f05f751d650972b3b74ff826afc2b
haoshan <1044917411@qq.com>
2019-07-31 17:10:34 +0800

    初步todolist

---
+++ src/ToDoList.js
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from 'react' // Fragment 占位符标签
+import './style.css'
+
+class ToDoList extends Component {
+  // 每个类都要有 constructor 接受react的组件中的 props, super 继承
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: ['learning', 'read book'],
+    }
+  }
+
+  render() {
+    return (<Fragment>
+      <div>
+        <h1>plan something</h1>
+        {/* 下面是一个input框 */}
+        <label htmlFor="insert">请输入内容</label>
+        <input type="text"
+          id='insert'
+          className='input'
+          value={this.state.inputValue}
+          onChange={this.handleChangeInput.bind(this)} />
+        <button onClick={this.handleClick.bind(this)}>提交</button>
+      </div>
+      <ul>
+        {this.state.list.map((item, index) => {
+          return <li key={index}
+            onClick={this.handlDel.bind(this, index)}
+            dangerouslySetInnerHTML={{ __html: item }}
+          ></li>
+        })}
+      </ul>
+    </Fragment>)
+  }
+  handleChangeInput(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleClick(e) {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: '',
+    })
+  }
+  handlDel(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list: list
+    })
+  }
+}
+
+export default ToDoList
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; // jsx的语法需要使用react 编译
+import ReactDOM from 'react-dom'; // 编译组件到dom 上显示元素
+import ToDoList from './ToDoList' // 引入组件
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render( < ToDoList / > , document.getElementById('root'));
