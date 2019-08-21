05625c41f2cfb52359fdb65a601429a5d3766fc7
haoshan <1044917411@qq.com>
2019-08-21 10:42:37 +0800

    css 动画

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'; // Component 从react 中解构,相当于 Component = React.Component
+import React, { Component, Fragment } from 'react';
+import './style.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
   render() {
-    return (
-      <div>
-        hello dell li
-      </div >
-    );
+    return (<Fragment>
+      <div className={this.state.status ? 'hidden' : 'show'}>class样式更改</div>
+      <button onClick={this.handleClick}>toggle</button>
+    </Fragment>)
+  }
+  handleClick() {
+    this.setState(() => {
+      return {
+        status: this.state.status ? false : true
+      }
+    })
   }
 }
 
-export default App;
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'; // jsx的语法需要使用react 编译
 import ReactDOM from 'react-dom'; // 编译组件到dom 上显示元素
-import ToDoList from './ToDoList' // 引入组件
+import App from './App.js' // 引入组件
 
-ReactDOM.render( < ToDoList / > , document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
